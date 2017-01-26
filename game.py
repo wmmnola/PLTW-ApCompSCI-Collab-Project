1f8405369b0c8514f4a7bed86f470be10949bc80
agentnola <wmmnola@gmail.com>
2017-01-26 13:16:57 -0600

    added display_hand. Which displayes a given hand

--- game.py
+++ game.py
@@ -13,6 +13,7 @@ def game_debug():
     # user_prompt()
     deck = generate_deck()
     draw_card(player_hand, deck)
+    print(display_hand(player_hand))
 
 
 
@@ -33,10 +34,8 @@ def generate_deck():
 
 
 def draw_card(hand, deck):
-    # TODO implement logic to append a random card from deck to a hand
-    # TODO encapsulate this function under one draw_card() function
     size = len(deck)-1
-    index = random.randint(0,size)
+    index = random.randint(0, size)
     card = deck[index]
     del deck[index]
     hand.append(card)
@@ -51,6 +50,14 @@ def ai_draw_card():
 def game_end():
     # TODO determine if we need a game_end() function, wouldnt a return work?
     pass
+
+
+def display_hand(hand):
+    """Takes a hand array, returns a string to be printed"""
+    str_hand = ""
+    for x in hand:
+        str_hand += "%s " % x.real_value
+    return str_hand
 
 
 def user_prompt():
