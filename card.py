c0dd3cfadd5fec077280cebd4bcb8e014d85ade1
agentnola <agentnola>
2017-01-26 08:15:13 -0600

    Added generate_deck() function

--- card.py
+++ card.py
@@ -3,5 +3,12 @@
 
 
 class Card(object):
-    def __init__(self, id):
+    def __init__(self, id, passedvalue):
         self.id = id
+        self.passed_value = passedvalue
+
+    def get_real_value(self):
+        if self.passed_value >= 11:
+            self.real_value = 10
+        else:
+            self.real_value = self.passed_value
